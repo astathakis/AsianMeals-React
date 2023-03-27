c3b8a4d08b26d6838b8e7623a47ba9a8015ac04c
tasos <tasos_stathakis@yahoo.com>
2023-03-28 00:50:40 +0300

    using contextapi

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,13 +1,26 @@
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartBtn.module.css'
+import CartContext from '../../store/cart-context'
+import { useContext } from 'react'
 const HeaderCartBtn = (props) => {
+  const cartCtx = useContext(CartContext)
+  /*============================================================
+  use reduce method to transform an array to a single value
+
+  adding an item into the items array / amount is the 
+  actual number of items and not the length of the array
+  ==========================================================*/
+  const numOfCartItems = cartCtx.items.reduce((tot, curr) => {
+    return tot + curr.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   )
 }
--- src/components/Meals/MealsItem/MealsItemForm.js
+++ src/components/Meals/MealsItem/MealsItemForm.js
@@ -1,10 +1,35 @@
+import { useState, useRef } from 'react'
 import Input from '../../UI/Input'
 import classes from './MealsItemForm.module.css'
 
 const MealsItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+
+  const amountInputRef = useRef()
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const enteredAmount = amountInputRef.current.value
+    //value comes always as string so
+    const enteredAmountNum = +enteredAmount
+    //validation on amount
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false)
+      return
+    }
+    props.onAddToCart(enteredAmountNum)
+  }
+
+  //using refs instead of two-way-binding
+  //custom component ref needs workout
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount' + props.id,
@@ -16,6 +41,7 @@ const MealsItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   )
 }
