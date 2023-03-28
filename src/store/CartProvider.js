d9f101d4b1769ebaae7369b08ff798043b8a37cf
tasos <tasos_stathakis@yahoo.com>
2023-03-29 01:40:53 +0300

    add remove functionality

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,14 +9,63 @@ const defaultCartState = {
 //using reducer outside of functional component no need to be rerendered
 const cartReducer = (state, action) => {
   if (action.type === 'ADD_ITEM') {
-    const updatedItems = state.items.concat(action.item)
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
+
+    //========================================================================
+    //check if item already in cart - display once, update amount only
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+
+    const existingCartItem = state.items[existingCartItemIndex]
+
+    let updatedItems
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+
+    //========================================================================
+
+    // const updatedItems = state.items.concat(action.item)
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     }
   }
+
+  //=========================removeItem=========================================
+  if (action.type === 'REMOVE_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    )
+    const existingItem = state.items[existingCartItemIndex]
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price
+    let updatedItems
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => {
+        return item.id !== action.id
+      })
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
+
   return defaultCartState
 }
 
@@ -30,7 +79,9 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'ADD_ITEM', item: item })
   }
 
-  const removeItemFromCartHandler = () => {}
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id: id })
+  }
 
   const cartContext = {
     items: cartState.items,
